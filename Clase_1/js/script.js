698642e87f87d2fb908e253985a23b66f069357e
neguma <guma.nicolas@gmail.com>
2021-12-21 16:30:15 -0300

    clase1

---
+++ Clase_1/js/script.js
@@ -0,0 +1,30 @@
+// var nombreApellido;
+// const NOMBRE;
+
+//COMENTARIO DE LINEA
+/* COMENTARIO */
+
+/*
+var numero1 = 30;
+var numero2 = 50;
+var suma = numero1+numero2;
+
+let nombre = "Nicolas"
+let apellido = "Guma"
+let nombreCompleto = nombre +" "+apellido
+
+let precioBruto = 1000;
+const IVA =  0.21;
+
+let ivaProducto = precioBruto * IVA;
+*/
+
+// let nombre = prompt("Ingrese su nombre");
+// let mensaje = console.log("hola", nombre);
+
+// let alerta = alert("Quiero milanesa")
+
+let nombre = prompt("Ingresá tu nombre:")
+alert("Hola,"+" "+nombre +"!")
+let edad = prompt("Ingresá tu edad:")
+alert(nombre +","+" "+"naciste en el año"+" "+(2021-parseInt(edad)))
