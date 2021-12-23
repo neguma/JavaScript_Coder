97b95f114614468ef92dfe5516dabb766309a539
Nicolas Ezequiel Guma <nicolas.guma@policia.gcba.gov.ar>
2021-12-22 21:40:26 -0300

    clase2

---
+++ clase 2/js/script.js
@@ -0,0 +1,23 @@
+/*let numero = 3;
+
+if (numero <= 1) {
+  console.log("OK");
+} else {
+  console.log("ERROR");
+}
+*/
+
+/*let edad = parseInt(prompt("Ingrese su edad:"));
+let edadAdulta = (edad>=13);
+
+if (edad < 6) {
+  alert("No puede pasar a ver esta película.");
+} else if (edadAdulta) {
+  alert("Puede entrar acompañado.");
+} else {
+  alert("Bienvenido.");
+}
+*/
+
+let valor = 12;
+let comparacion = (valor==12);
